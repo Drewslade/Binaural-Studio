0a6fc9786b1f13d0b59bccdb41ff90e66ae8e641
drew-dmg <drew@digitalmiddleground.com>
2026-07-21 09:18:01 -0500

    Restrict draft content access

--- collections/access.ts
+++ collections/access.ts
@@ -4,6 +4,16 @@ export const anyone: Access = () => true;
 
 export const authenticated: Access = ({ req: { user } }) => Boolean(user);
 
+export const publishedOrAuthenticated: Access = ({ req: { user } }) => {
+  if (user) return true;
+
+  return {
+    _status: {
+      equals: "published",
+    },
+  };
+};
+
 export const contentAccess = {
   read: anyone,
   create: authenticated,
